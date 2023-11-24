5eb5ede8bc6e2ed9d96dbbc38c95d15f2a42297e
KarinPellegrini <karinfarnholc@gmail.com>
2023-11-23 23:39:35 -0300

    agregando la query para que filtre por categoria

--- xtremeRental/src/main/java/com/dh/xtremeRental/repository/IProductoRepository.java
+++ xtremeRental/src/main/java/com/dh/xtremeRental/repository/IProductoRepository.java
@@ -16,6 +16,7 @@ public interface IProductoRepository extends JpaRepository<Producto,Integer> {
     @Query(value = "SELECT * FROM producto where id=:id ", nativeQuery = true)
     Optional<Producto> findById(Integer id);
 
+    //Cambio realizado por K
     @Query(value = "SELECT * FROM producto WHERE nombre_producto LIKE %:palabra% OR descripcion_producto LIKE %:palabra% OR categoria LIKE %:palabra% order by id asc"  , nativeQuery = true)
     List<Producto> findByPalabra(@Param("palabra") String palabra);
 
